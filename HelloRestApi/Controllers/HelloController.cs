10c5a12864df6a4e1b362f484ae1dfbdf04e3225
Radosław Radomski <pryq11@gmail.com>
2019-04-11 14:15:54 +0200

    Add project files.

---
+++ HelloRestApi/Controllers/HelloController.cs
@@ -0,0 +1,57 @@
+﻿using HelloRestApi.Model;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HelloRestApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HelloController : ControllerBase
+    {
+        private static List<Hello> _messeges = new List<Hello>();
+
+        // GET api/hello
+        [HttpGet]
+        public ActionResult<IEnumerable<Hello>> GetHello()
+        {
+            return Ok(_messeges);
+        }
+
+        // GET api/hello/5
+        [HttpGet("{id}")]
+        public ActionResult<Hello> Get(int id)
+        {
+            if (_messeges.Count < id+1)
+            {
+                return StatusCode(400, "Bad request");
+            }
+
+            return Ok(_messeges[id]);
+        }
+
+        // POST api/hello
+        [HttpPost]
+        public ActionResult Post([FromBody] Hello value)
+        {
+            _messeges.Add(value);
+            return Created(new Uri(Request.Path + "/" + (_messeges.Count-1).ToString(),UriKind.Relative),value);
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public ActionResult Put(int id, [FromBody] string value)
+        {
+            return StatusCode(400, "Bad request");
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public ActionResult Delete(int id)
+        {
+            return StatusCode(400, "Bad request");
+        }
+    }
+}
---
+++ HelloRestApi/Model/Hello.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HelloRestApi.Model
+{
+    public class Hello
+    {
+        public string Message { get; set; }
+        public string Author { get; set; }
+
+        public Hello(string message, string author)
+        {
+            Message = message;
+            Author = author;
+        }
+    }
+}
